8b6dc0a63915f8f65a326813d740d9b352c1ca50
DavidJones <qowera@gmail.com>
2025-04-13 11:30:11 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Meta, { generate } from '@/components/Meta'
 
 const { generateMetadata, metaProps } = generate({
   title: 'Open APIs',
-  description: 'This service collects and caches commonly used public OPENAPIs to facilitate developer access. It provides caching and forwarding services for commonly used public APIs, making it easier for developers to quickly access them.',
+  description:
+    'This service collects and caches commonly used public OPENAPIs to facilitate developer access. It provides caching and forwarding services for commonly used public APIs, making it easier for developers to quickly access them.',
 })
 
 export { generateMetadata }
