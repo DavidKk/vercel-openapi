8be798f45ac4a68a4d512c57c340b56014ccf3cb
DavidJones <qowera@gmail.com>
2025-04-13 11:30:11 +0800

    first commit

--- app/holiday/page.tsx
+++ app/holiday/page.tsx
@@ -5,7 +5,7 @@ export default async function CalendarPage() {
   const holidays = await listHoliday()
 
   return (
-    <div className="flex flex-col items-center w-screen h-[calc(100vh-60px-64px)] relative bg-gray-50 pt-12 px-12">
+    <div className="flex flex-col items-center w-screen min-h-[calc(100vh-60px-64px)] relative bg-gray-50 py-12 px-12">
       <Calendar holidays={holidays} />
     </div>
   )
